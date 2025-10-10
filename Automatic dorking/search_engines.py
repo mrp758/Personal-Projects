bc06da66fb74af18d7adaacceef263c8a41565c3
mrp758 <74987993+mrp758@users.noreply.github.com>
2025-10-10 21:18:30 +0300

    Update search_engines.py
    Updated search engine api for google

--- Automatic dorking/search_engines.py
+++ Automatic dorking/search_engines.py
@@ -1,5 +1,5 @@
 # For more search engines heres a refrence to a good web browser that holds a lot of search engines  https://4get.ca/settings
-GOOGLE_SEARCH_BASE_URL = "https://4get.ca/api/v1/web?&scraper=google&nsfw=yes"
+GOOGLE_SEARCH_BASE_URL = "https://4get.ca/api/v1/web?&scraper=google_api&nsfw=yes"
 DUCK_DUCK_GO_SEARCH_BASE_URL = "https://4get.ca/api/v1/web?&scraper=ddg&nsfw=yes"
 YANDEX_SEARCH_BASE_URL = "https://4get.ca/api/v1/web?&scraper=yandex&nsfw=yes"
 START_PAGE_SEARCH_BASE_URL = "https://4get.ca/api/v1/web?&scraper=startpage&nsfw=yes"
