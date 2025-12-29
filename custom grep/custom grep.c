d76815fa5993ac080f9687926ee4741fa04a0fde
mrp758 <74987993+mrp758@users.noreply.github.com>
2025-12-29 14:01:50 +0200

    Implement custom grep functionality in C
    Uploaded custom grep functionality in C

---
+++ custom grep/custom grep.c
@@ -0,0 +1,168 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+
+char readFile(char ** tempStore,char* fileName);
+char arrayOfStrings(char * stringsFromFile, char array[][256]);
+char returnStrings(int indexPosition,char content[][256], char * foundStrings[], char * phrase);
+char returnStringsViaSpecialChars(int indexPosition,char content[][256], char * foundStrings[], char * phrase);
+
+
+// This is ANSI usage in C
+// printf("\033[31mYour text here\033[0m"); // - red color
+//  printf("\033[32mHello, World\033[0m\n");  // - Green
+//  printf("\033[34mThis text is blue\033[0m\n"); // - Blue
+
+
+int main(){
+
+    char content[10][256] = {0};
+    char fileName[] = "your_file_name";
+    char phrase[] = "your word/special character";
+    char string[25];
+    char * tempStore = NULL;
+    char * foundStrings[256] = {0};
+    readFile(&tempStore,fileName);
+    int indexPosition = arrayOfStrings(tempStore,content);
+    int indexStr = 0, match=0, lastPos=0, lengthOfPhrase = strlen(phrase);
+    returnStrings(indexPosition,content,foundStrings,phrase);
+    returnStringsViaSpecialChars(indexPosition,content,foundStrings,phrase);
+    
+    
+  
+    return 0;
+}
+
+
+
+
+char readFile(char ** readContents,char * fileName){
+
+    FILE * file;
+
+    size_t bytesRead;
+
+
+    *readContents =  (char*)malloc(255 * sizeof(char));
+
+    file = fopen(fileName,"r");
+
+    if(file == NULL){
+
+        perror("Can't open file, Check if file in the same directory/exsists");
+    }
+
+
+    bytesRead = fread(*readContents, 1, 254, file);
+
+    (*readContents)[bytesRead] = '\0';
+      
+    fclose(file);
+
+    return 0;
+}
+
+
+
+char arrayOfStrings(char * stringsFromFile, char array[][256]){
+    int indexStr=0,indexPost=0;
+    for(int index=0; index < strlen(stringsFromFile); index++){
+        if(stringsFromFile[index] == '\n'){
+            array[indexPost][indexStr] = '\0';
+            indexStr=0;
+            indexPost++;
+            continue;
+        }
+        array[indexPost][indexStr] = stringsFromFile[index];
+        indexStr++;
+    }
+    
+
+    return indexPost;
+
+}
+
+
+char returnStrings(int indexPosition,char content[][256], char * foundStrings[], char * phrase){
+    int indexStr = 0, match=0, lengthOfPhrase = strlen(phrase);
+    char string[25];
+    int indexStrF=0,indexPostF=0;
+    
+       for(int i=0; i <= indexPosition; i++){
+
+        if(strlen(content[i]) > 0){
+            indexStr=0;
+            memset(string, '\0', 25);
+
+        for(int j=0; j < strlen(content[i]); j++){
+
+            string[indexStr] = content[i][j];
+            indexStr++;
+            string[indexStr] = '\0';
+
+            if(strlen(string) == strlen(phrase)  && strcmp(string,phrase)==0){
+                foundStrings[indexPostF] = content[i];
+                indexPostF++;
+            }
+
+            else if(content[i][j] == ' '){
+                string[indexStr] = '\0';
+                indexStr=0;
+                memset(string, '\0', 25);
+            }
+        }
+    }
+
+    }
+
+    for (int i = 0; i < indexPostF; i++)
+    {   
+        printf("\033[32m%s\033[0m\n",foundStrings[i]);
+    }
+    
+    
+    return 0;
+}
+
+
+char returnStringsViaSpecialChars(int indexPosition,char content[][256], char * foundStrings[], char * phrase){
+
+    int indexStr = 0, match=0, lengthOfPhrase = strlen(phrase);
+    int indexPostF = 0;
+    char string[25];
+        for(int i=0; i <= indexPosition; i++){
+
+        if(strlen(content[i]) > 0){
+            indexStr=0;
+            memset(string, '\0', 25);
+
+            for(int j=0; j < strlen(content[i]); j++){
+
+                string[indexStr] = content[i][j];
+                indexStr++;
+                string[indexStr] = '\0';
+                
+                if(strlen(string) == strlen(phrase) && ispunct(content[i][j]) && strcmp(string,phrase)==0){
+                        foundStrings[indexPostF] = content[i];
+                        indexPostF++;
+                }
+
+                else{
+
+                    indexStr=0;
+                    memset(string, '\0', 25);
+                }
+                
+            }
+        }
+    }
+
+    for (int i = 0; i < indexPostF; i++)
+    {
+        printf("\033[34m%s\033[0m\n",foundStrings[i]);
+    }
+
+    return 0;
+}
